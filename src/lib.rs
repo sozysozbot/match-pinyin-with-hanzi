436edc57d889f2853558969d6b778338a3215547
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-01-07 08:08:46 +0900

    0.1.4: bump up the version and distinguish single quotes from apostrophes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,4 @@
-#[cfg(test)]
-mod tests {
-    use crate::match_pinyin_with_hanzi;
-    #[test]
-    fn it_works() {
-        match_pinyin_with_hanzi("Nǐ qù nǎli?", "你去哪里？").unwrap();
-    }
-}
+#![warn(clippy::pedantic, clippy::nursery)]
 
 /// ```
 /// use match_pinyin_with_hanzi::match_pinyin_with_hanzi;
@@ -15,12 +8,23 @@ mod tests {
 /// // This sample sentence is taken from Wiktionary: https://en.wiktionary.org/w/index.php?title=%E4%B8%80%E9%BB%9E%E5%85%92&oldid=60782800
 /// match_pinyin_with_hanzi("Jiù wèi zhème yīdiǎnr shìr shēngqì, zhídàng de ma?", "就為這麼一點兒事兒生氣，值當的嗎？").unwrap();
 /// ```
+/// 
+/// # Panics
+/// Panics when
+/// - hanzi runs out
+/// 
+/// # Errors
+/// Returns `Err` when
+/// - hanzi's pinyin candidate does not match with the given pinyin
 pub fn match_pinyin_with_hanzi(pinyin_str: &str, hanzi_str: &str) -> Result<(), String> {
     use pinyin::ToPinyinMulti;
     use pinyin_parser::PinyinParser;
 
     let mut hanzi_iter = hanzi_str.chars();
-    for pinyin in PinyinParser::strict(pinyin_str) {
+    for pinyin in PinyinParser::new()
+        .with_strictness(pinyin_parser::Strictness::StrictAndSeparateApostropheFromCurlyQuote)
+        .parse(pinyin_str)
+    {
         let (hanzi, candidates) = loop {
             let hanzi = hanzi_iter.next().unwrap_or_else(|| {
                 panic!(
@@ -53,8 +57,7 @@ pub fn match_pinyin_with_hanzi(pinyin_str: &str, hanzi_str: &str) -> Result<(), 
                         break;
                     }
                     return Err(format!(
-                        "expected 儿 or 兒 because of the rhotic pinyin {}, but instead found a Chinese character {}",
-                        pinyin, expect_儿
+                        "expected 儿 or 兒 because of the rhotic pinyin {pinyin}, but instead found a Chinese character {expect_儿}",
                     ));
                 }
             }
@@ -64,11 +67,19 @@ pub fn match_pinyin_with_hanzi(pinyin_str: &str, hanzi_str: &str) -> Result<(), 
             }
 
             return Err(format!(
-                "{} not found within candidates {:?} possible for the Chinese character {}. Encountered this while matching `{}` with `{}`.",
-                pinyin, candidates, hanzi, pinyin_str, hanzi_str
+                "{pinyin} not found within candidates {candidates:?} possible for the Chinese character {hanzi}. Encountered this while matching `{pinyin_str}` with `{hanzi_str}`.",
             ));
         }
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::match_pinyin_with_hanzi;
+    #[test]
+    fn it_works() {
+        match_pinyin_with_hanzi("Nǐ qù nǎli?", "你去哪里？").unwrap();
+    }
 }
